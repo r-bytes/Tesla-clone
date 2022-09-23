15ef23d255f0210feedecf2a19ac578f32d42c89
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-23 03:03:42 +0200

    first commit

---
+++ src/components/Section.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
+const Section = ({title, description, leftBtnText, rightBtnText, backgroundImage}) => {
+    return (
+        <Wrapper bgImage={backgroundImage}>
+            <ItemText>
+                <h1> {title} </h1>
+                <p> {description} </p>
+            </ItemText>
+            <ButtonGroup>
+                <Buttons>
+                    <LeftButton>
+                        {leftBtnText}
+                    </LeftButton>
+                    {rightBtnText &&
+                        <RightButton>
+                            {rightBtnText}
+                        </RightButton>}
+                </Buttons>
+                <DownArrow src="/images/down-arrow.svg" />
+            </ButtonGroup>
+        </Wrapper>
+    )
+}
+
+export default Section
+
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: ${props => `url("/images/${props.bgImage}")`};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    
+`
+
+const Buttons = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 1.5rem;
+    margin-bottom: 1.875rem;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 2.5rem;
+    width: 16rem;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: .2rem;
+    opacity: .85;
+    text-transform: uppercase;
+    font-size: .75rem;
+    cursor: pointer;
+
+`
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    opacity: .65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    margin-top: 1.25rem;
+    height: 2.5rem;
+    animation: animateDown infinite 1.5s;
+`
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export {default as Header} from "./Header.jsx"
+export {default as Home} from "./Home.jsx"
+export {default as Section} from "./Section.jsx"
