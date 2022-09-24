bc6af3f913a18045ee8db1f41f689cd238e4befd
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-24 02:25:18 +0200

    Wrapped Home in custom div for modal effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { Header, Home } from "./components";
+import { Header, Home } from "./components"
+import { useStateContext } from "./context/StateContext"
+import styled from "styled-components"
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
-        <Home />
-    </div>
-  );
+    const {showBurger} = useStateContext()
+    console.log(showBurger)
+
+    return (
+        <div>
+            <Header />
+            <Wrapper show={showBurger}>
+                <Home />
+            </Wrapper>
+        </div>
+    );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+    filter: ${props => props.show ? "blur(8px)" : ""};
+    -webkit-filter: ${props => props.show ? "blur(8px)" : ""};
+`
