6a3be7b02b3e51b7cd7ac92303a2a5d53e6b5c88
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-24 02:24:17 +0200

    Added Provider to children

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StateContext } from "./context/StateContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <App />
+    <StateContext>
+        <App />
+    </StateContext>
 
 );
